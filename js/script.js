060e419d28361d65c7cd5c65f9ad3f88eae761c4
eloisa <eloisantunesp@gmail.com>
2021-11-22 19:42:04 -0300

    config-ecommerce-loja-games

--- js/script.js
+++ js/script.js
@@ -1,32 +1,20 @@
+/*$(document).ready(function(){
+    $("#slide img:eq(0)").addClass('active')(
 
-var carouselInner = 2;
-showSlides(carouselInner);
+    function slide(){
+        if($('.ativo').next().size()){
+            $('.ativo').fadeOut().removeClass('ativo').next().fadeIn().addClass("ativo"))
 
-function plusSlides(n) {
-    showSlides(carouselInner += n);
-}
+        }else{
+            $('.ativo').fadeOut().removeClass('ativo');
+            $('#slide img:eq(0)').fadeIn().addClass("ativo");
 
-function currentSlide(n) {
-    showSlides(carouselInner = n);
-}
-
-function showSlides(n) {
-    var i;
-    var slides = document.getElementById("carouselIndicators");
-    var dots = document.getElementByClassName("carousel-inner");
-
-    if (n > slides.length) {carouselInner=1}
-    if (n < 1) {carouselInner = slides.length}
-    for (1=0; i<slides.length; i++) {
-        slides[i].style.display = "none";
+        }
     }
-    for (i=0; i < dots.length; i++) {
-        dots[i].className= dots[i].className.replace("active", "");
-    }
-    slides[carouselInner -1].style.display = "block";
-    dots[carouselInner -1].className += "active";
-} 
-
-$('.carousel').carousel({
-    interval: 2000
-  }) 
+});
+$(document).ready(function() {
+    $("#flip").click(function() {
+        $("#panel").slideToggle("slow");    
+    });
+});
+*/
